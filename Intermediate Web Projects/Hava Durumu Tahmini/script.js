b65bbd9b801ddccecff3999ab37eeb21f74dba44
Emre Mert <emre.mrt98@gmail.com>
2022-06-04 21:09:08 +0300

    Emre Mert

---
+++ Intermediate Web Projects/Hava Durumu Tahmini/script.js
@@ -0,0 +1,34 @@
+const buton = document.querySelector(".btn");
+
+
+
+buton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const girdi = document.querySelector(".inputText").value;
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + girdi + "&appid=b6bf92d95010884e91044ca4acc037d3&lang=tr"
+
+    fetch(url).then(weather => {
+        return weather.json()
+    }).then(displayResult)
+
+
+});
+// 
+const displayResult = (res) => {
+    console.log(res);
+    const sehirAdi = document.getElementById("sehir");
+    sehirAdi.innerText = `${res.name}, ${res.sys.country}`;
+    const sicaklik = document.getElementById("sicaklik");
+    sicaklik.innerText = degerDonus(res.main.temp);
+    const havaDurumu = document.getElementById("havaDurumu");
+    havaDurumu.innerText = res.weather[0].description;
+    const hissedilenDeger = document.getElementById("hissedilen");
+    hissedilenDeger.innerText = degerDonus(res.main.feels_like);
+
+
+}
+
+function degerDonus(sicaklik) {
+    return `${(sicaklik - 273).toFixed(0)}`;
+}
